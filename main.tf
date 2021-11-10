8aaaa0f8665b31872f5230c790e1a2ea7d522da9
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-10 11:43:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,40 @@ terraform {
 }
 
 provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = "AKIARGZPFHSSJRTG2FB2"
-  secret_key = "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"
+  region  = "ap-southeast-1"
+  version = "~> 2.36.0"
 }
 
 #Aws Instance
-resource "aws_instance" "windows" {
-   ami = "ami-0bd2beaddfd6419fc"
-   instance_type = "t2.micro"
-   tags = {
-     Name = "windows"
-   }
+#/*resource "aws_instance" "windows" {
+ #  ami = "ami-0bd2beaddfd6419fc"
+  # instance_type = "t2.micro"
+ #  tags = {
+  #   Name = "windows"
+ #  }
+#}
+# Build the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  tags = {
+    Name      = "Vpc"
+    Terraform = "true"
+  }
+}
+# Build route table 1
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable1"
+    Terraform = "true"
+  }
+}
+# Build route table 2
+resource "aws_route_table" "route_table2" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable2"
+    Terraform = "true"
+  }
 }
