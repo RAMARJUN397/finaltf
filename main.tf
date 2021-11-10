e47292111eabce963029d32e2cdd3d00da5c0468
mallikarjunav747@gmail.com <mallikarjunav747@gmail.com>
2021-11-10 11:21:50 +0530

    executed

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">=0.12.13"
+}
+
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "AKIARGZPFHSSJRTG2FB2"
+  secret_key = "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"
+}
+
+#Aws Instance
+resource "aws_instance" "windows" {
+   ami = "ami-0bd2beaddfd6419fc"
+   instance_type = "t2.micro"
+   tags = {
+     Name = "windows"
+   }
+}
