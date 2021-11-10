3c45c918514bda0f22db9799dda84c7a3e9f00a0
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-10 11:26:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ terraform {
 
 provider "aws" {
   region     = "ap-southeast-1"
-  access_key = "AKIARGZPFHSSJRTG2FB2"
-  secret_key = "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"
+  #access_key = "AKIARGZPFHSSJRTG2FB2"
+  #secret_key = "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"
 }
 
 #Aws Instance
